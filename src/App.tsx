0159c69357453b32b1ff98d72495c6f9ea43a59c
GastonSKL <gastonlskamlec@gmail.com>
2022-12-02 15:56:56 -0300

    Se agregan componentes a ser utilizados de meterial ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,17 @@
+import { useState } from "react"
+import { useQuery } from "react-query";
 
+import Drawer from '@material-ui/core/Drawer';
+import LinearProgress from "@material-ui/core/LinearProgress";
+import { Grid } from "@material-ui/core";
+import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
+import Badge from "@material-ui/core/Badge";
+
+import { Wrapper } from './AppStyles';
+
+import { CartItemType} from './Types/types'
+
+const getCharacter = async():Promise<CartItemType[]> => await(await fetch('https://www.swapi.tech/api/people')).json();
 
 function App() {
   
---
+++ src/Types/types.tsx
@@ -0,0 +1,7 @@
+export type CartItemType = {
+    id:number;
+    name:string;
+    description: string;
+    height:number;
+
+}
